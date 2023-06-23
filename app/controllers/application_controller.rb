847ce4ebdc35b119ba0b562c424591b438904381
EC2 Default User <ec2-user@ip-172-31-1-76.ap-northeast-1.compute.internal>
2023-06-23 06:22:26 +0000

    relay point1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    @user_path
+    #一時的な措置 最終的に/user/:id に変更する
+    #@user_pathで目的のページへ遷移したがなぜそうなるのかわからない
+  end
+
+#  def after_sign_our_path_for(resource)
+#    homes_top_path
+#  end
+#デフォルトの遷移先がroot_pathのため、上記コードは必要なかった
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+    #params.require(:book).permit(:title, :body)
+    #.require(:book)をつけるとparams missingエラーが出るので、推奨される方法ではないらしいがいったん削除
+    #formがBook.newを受けとれていないっぽい？ いろいろ試したがダメなので
+
+  end
+
+end
